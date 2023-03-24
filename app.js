63bf794a6f4a9eab7a78acd7982a851d28c87f93
isaacmou <126214425+isaacmou@users.noreply.github.com>
2023-03-24 20:19:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,42 +6,7 @@ function showEnglish() {
     document.getElementById("chiInput").style.display = "none";
     document.getElementById("engVerwithdistrict").style.display = "none";
     <!--Show english district button-->
-    document.getElementById("btn1").style.display = "";
-    document.getElementById("btn2").style.display = "";
-    document.getElementById("btn3").style.display = "";
-    document.getElementById("btn4").style.display = "";
-    document.getElementById("btn5").style.display = "";
-    document.getElementById("btn6").style.display = "";
-    document.getElementById("btn7").style.display = "";
-    document.getElementById("btn8").style.display = "";
-    document.getElementById("btn9").style.display = "";
-    document.getElementById("btn10").style.display = "";
-    document.getElementById("btn11").style.display = "";
-    document.getElementById("btn12").style.display = "";
-    document.getElementById("btn13").style.display = "";
-    document.getElementById("btn14").style.display = "";
-    document.getElementById("btn15").style.display = "";
-    document.getElementById("btn16").style.display = "";
-    document.getElementById("btn17").style.display = "";
-    document.getElementById("btn18").style.display = "";
-    document.getElementById("btn19").style.display = "none";
-    document.getElementById("btn20").style.display = "none";
-    document.getElementById("btn21").style.display = "none";
-    document.getElementById("btn22").style.display = "none";
-    document.getElementById("btn23").style.display = "none";
-    document.getElementById("btn24").style.display = "none";
-    document.getElementById("btn25").style.display = "none";
-    document.getElementById("btn26").style.display = "none";
-    document.getElementById("btn27").style.display = "none";
-    document.getElementById("btn28").style.display = "none";
-    document.getElementById("btn29").style.display = "none";
-    document.getElementById("btn30").style.display = "none";
-    document.getElementById("btn31").style.display = "none";
-    document.getElementById("btn32").style.display = "none";
-    document.getElementById("btn33").style.display = "none";
-    document.getElementById("btn34").style.display = "none";
-    document.getElementById("btn35").style.display = "none";
-    document.getElementById("btn36").style.display = "none";
+    
     
 
 }
@@ -54,42 +19,7 @@ function showChinese() {
     document.getElementById("chiInput").style.display = "none";
     document.getElementById("engVerwithdistrict").style.display = "none";
     <!--show chinese button-->
-    document.getElementById("btn1").style.display = "none";
-    document.getElementById("btn2").style.display = "none";
-    document.getElementById("btn3").style.display = "none";
-    document.getElementById("btn4").style.display = "none";
-    document.getElementById("btn5").style.display = "none";
-    document.getElementById("btn6").style.display = "none";
-    document.getElementById("btn7").style.display = "none";
-    document.getElementById("btn8").style.display = "none";
-    document.getElementById("btn9").style.display = "none";
-    document.getElementById("btn10").style.display = "none";
-    document.getElementById("btn11").style.display = "none";
-    document.getElementById("btn12").style.display = "none";
-    document.getElementById("btn13").style.display = "none";
-    document.getElementById("btn14").style.display = "none";
-    document.getElementById("btn15").style.display = "none";
-    document.getElementById("btn16").style.display = "none";
-    document.getElementById("btn17").style.display = "none";
-    document.getElementById("btn18").style.display = "none";
-    document.getElementById("btn19").style.display = "";
-    document.getElementById("btn20").style.display = "";
-    document.getElementById("btn21").style.display = "";
-    document.getElementById("btn22").style.display = "";
-    document.getElementById("btn23").style.display = "";
-    document.getElementById("btn24").style.display = "";
-    document.getElementById("btn25").style.display = "";
-    document.getElementById("btn26").style.display = "";
-    document.getElementById("btn27").style.display = "";
-    document.getElementById("btn28").style.display = "";
-    document.getElementById("btn29").style.display = "";
-    document.getElementById("btn30").style.display = "";
-    document.getElementById("btn31").style.display = "";
-    document.getElementById("btn32").style.display = "";
-    document.getElementById("btn33").style.display = "";
-    document.getElementById("btn34").style.display = "";
-    document.getElementById("btn35").style.display = "";
-    document.getElementById("btn36").style.display = "";
+   
 
 }
 function Area1() {
